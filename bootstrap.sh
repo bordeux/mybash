8f559bc4a423f8a4be3b18767078ad444aa76879
Chris Bednarczyk <chris@tourradar.com>
2019-07-15 11:35:33 +0200

    Hello world 🐧

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,12 @@
 
 export MY_BASH_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+eval $(cat ${HOME}/.env | sed 's/^/export /')
 
 # include all aliases
 source <(cat ${MY_BASH_PATH}/alias/*.sh)
 
 # include all fynctions
 source <(cat ${MY_BASH_PATH}/functions/*.sh)
+
+
