d83f0a4f0928e8431d46b898ff3ae84b6255466b
Chris Bednarczyk <chris@tourradar.com>
2019-07-16 14:15:47 +0200

    Hello world

--- alias/aws.sh
+++ alias/aws.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-alias ecrlogin="$(aws ecr get-login --no-include-email --region us-east-1)"
+
+ecrlogin()
+{
+    AWS_PROFILE=${1:-"default"}
+    LOGIN_SCRIPT=$(aws ecr get-login --no-include-email --region us-east-1 --profile ${AWS_PROFILE})
+    eval $LOGIN_SCRIPT
+}
+
