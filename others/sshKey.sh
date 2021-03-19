0ee07380c6675f63cf04335865e23f83cc055937
Chris Bednarczyk <chris@tourradar.com>
2021-03-19 13:41:09 +0100

    Support linux for ssh export

--- others/sshKey.sh
+++ others/sshKey.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa | base64)
+OS=$(uname -s)
+
+if [ "${OS}" = "Darwin" ]
+then
+  SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa | base64 )
+else
+  SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa | base64 -w 0)
+fi
+
 export BB_PIPELINES_SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}
